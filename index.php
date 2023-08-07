e6cc18bc2947866a354b1e2fe9fc0e6d93d9c3e4
Bruno Oliveira <bds.commus@gmail.com>
2023-08-06 22:25:07 -0300

    add phpunit

--- index.php
+++ index.php
@@ -5,25 +5,6 @@ interface BasicOperaration {
     public function getSecondNumber(): float;
 }
 
-class Sum implements BasicOperaration {
-
-    public function __construct(
-        private ?float $firstNumber,
-        private ?float $secondNumber,
-    ) {
-    }
-
-    public function calculate(): float
-    {
-        return $this->firstNumber + $this->secondNumber;
-    }
-
-    public function getSecondNumber(): float
-    {
-        return $this->secondNumber;
-    }
-}
-
 class Subtract implements BasicOperaration {
 
     public function __construct(
